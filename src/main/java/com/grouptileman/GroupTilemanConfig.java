c553d67d8c99643b1674bf414abf8c675777d0be
Nick <nicksv88@gmail.com>
2024-12-24 15:32:41 -0500

    Renaming

--- src/main/java/com/grouptileman/GroupTilemanConfig.java
+++ src/main/java/com/grouptileman/GroupTilemanConfig.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.grouptileman;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
-@ConfigGroup("example")
-public interface ExampleConfig extends Config
+@ConfigGroup("grouptileman")
+public interface GroupTilemanConfig extends Config
 {
 	@ConfigItem(
 		keyName = "greeting",
